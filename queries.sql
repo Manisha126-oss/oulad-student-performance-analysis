a18b7a491e47c2effa9750a943f9e24ec1f6d5bc
Manisha126-oss <manisha.94329@gmail.com>
2026-07-03 09:09:53 +0530

    Delete queries.sql

--- queries.sql
+++
@@ -1,9 +0,0 @@
-Question 1: Does disability status affect student outcomes?
-SELECT disability, final_result, COUNT(*) AS number_of_students
-FROM studentInfo
-GROUP BY disability, final_result;
-
-Question 2: Does region affect student outcomes?
-SELECT region, final_result, COUNT(*) AS number_of_students
-FROM studentInfo
-GROUP BY region, final_result;
