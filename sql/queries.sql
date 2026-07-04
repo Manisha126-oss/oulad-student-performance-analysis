cbb1f239545466cb39d944250031602cc2d58439
Manisha126-oss <manisha.94329@gmail.com>
2026-07-04 07:35:31 +0530

    Comment out questions in SQL queries

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,9 +1,14 @@
-Question 1: Does disability status affect student outcomes?
+-- Question 1: Does disability status affect student outcomes?
 SELECT disability, final_result, COUNT(*) AS number_of_students
 FROM studentInfo
 GROUP BY disability, final_result;
 
-Question 2: Does region affect student outcomes?
+-- Question 2: Does region affect student outcomes?
 SELECT region, final_result, COUNT(*) AS number_of_students
 FROM studentInfo
 GROUP BY region, final_result;
+
+-- Question 3: Does gender affect student outcomes?
+SELECT gender, final_result, COUNT(*) AS number_of_students
+FROM studentInfo
+GROUP BY gender, final_result;
